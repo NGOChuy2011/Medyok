5a62d31162a045232515eac59d79e74711cae493
NGOChuy2011 <boyhuy27@gmail.com>
2022-09-12 00:00:37 +0700

    okl

--- main.js
+++ main.js
@@ -1,62 +1,64 @@
 // show info header
 document.querySelector('.activer_find-us').addEventListener('click', function () {
-	this.parentElement.classList.toggle('open_find-us')
-	this.previousElementSibling.focus()
+    this.parentElement.classList.toggle('open_find-us')
+    this.previousElementSibling.focus()
 })
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~cart~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 const btn = document.querySelectorAll('#addcart')
 // console.log(btn)
-btn.forEach(function(button,index){
-    button.addEventListener('click',function(event){{
-        var btnItem = event.target
-        var product = btnItem.parentElement
-        var productImg = product.querySelector('img').src
-        var productName = product.querySelector('span').innerText
-        var productPrice = product.querySelector('.price_1').innerText
-        // console.log(productImg,productName,productPrice)
-        addcart(productImg,productName,productPrice)
-    }})
+btn.forEach(function (button, index) {
+    button.addEventListener('click', function (event) {
+        {
+            var btnItem = event.target
+            var product = btnItem.parentElement
+            var productImg = product.querySelector('img').src
+            var productName = product.querySelector('span').innerText
+            var productPrice = product.querySelector('.price_1').innerText
+            // console.log(productImg,productName,productPrice)
+            addcart(productImg, productName, productPrice)
+        }
+    })
 })
-function addcart(productImg,productName,productPrice){
+function addcart(productImg, productName, productPrice) {
     let countItem = 0;
     var addtr = document.createElement('tr')
     var cartItem = document.querySelectorAll('tbody tr')
 
-    for (var i=0;i<cartItem.length;i++){
+    for (var i = 0; i < cartItem.length; i++) {
         var productT = document.querySelectorAll('.name_prd')
-        if(productT[i].innerHTML == productName ){
+        if (productT[i].innerHTML == productName) {
             countItem++;
         }
     }
-    if(countItem > 0) {
+    if (countItem > 0) {
         let itemNumber = document.getElementById(productName).getAttribute('value');
         itemNumber++;
         document.getElementById(productName).setAttribute('value', itemNumber);
     } else {
-        var trcontent = '<tr class="cart-row"><td><img src="'+productImg+'" alt=""><span class= "name_prd">'+productName+'</span></td><td><span class ="price_prd">'+productPrice+'</span></td> <td> <input id="'+productName+'" class="cart-quantity-input" type="number" step="1" value="1" min="1"> </input>  </td>  <td><span class="delete-prd">Xoá</span></td></tr>'
+        var trcontent = '<tr class="cart-row"><td><img src="' + productImg + '" alt=""><span class= "name_prd">' + productName + '</span></td><td><span class ="price_prd">' + productPrice + '</span></td> <td> <input id="' + productName + '" class="cart-quantity-input" type="number" step="1" value="1" min="1"> </input>  </td>  <td><span class="delete-prd">Xoá</span></td></tr>'
         addtr.innerHTML = trcontent
         var cartTable = document.querySelector('tbody')
         // console.log(cartTable)
         cartTable.append(addtr)
     }
-   
+
     carttotal()
     deleteCart()
-   
-    console.log( );
+
+    console.log();
 }
 // --------------------total-price------------------
-function carttotal (){
+function carttotal() {
     var cartItem = document.querySelectorAll('tbody tr')
     var totalC = 0
     // console.log(cartItem.length)
-    for (var i=0;i<cartItem.length;i++){
+    for (var i = 0; i < cartItem.length; i++) {
         var inputValue = cartItem[i].querySelector('input').value
         // console.log(inputValue)
         var productPrice = cartItem[i].querySelector('.price_prd').innerHTML
         // console.log(productPrice)
-        totalA = inputValue*productPrice*1000
+        totalA = inputValue * productPrice * 1000
         // totalB = totalA.toLocaleString('de-DE')
         // console.log(totalB)
         totalC = totalC + totalA
@@ -64,41 +66,95 @@ function carttotal (){
     }
     var carttotalA = document.querySelector('.price-total span')
     carttotalA.innerHTML = totalC.toLocaleString('de-DE')
-    inputchange ()
+    inputchange()
     document.getElementsByClassName('item-numb')[0].textContent = i /= 1;
 }
 // --------------------Delete-cart------------------
-function deleteCart(){
+function deleteCart() {
     var cartItem = document.querySelectorAll('tbody tr')
-    for (var i=0;i<cartItem.length;i++){
-        var productT = document.querySelectorAll('.delete-prd') 
-        productT[i].addEventListener("click",function(event){
+    for (var i = 0; i < cartItem.length; i++) {
+        var productT = document.querySelectorAll('.delete-prd')
+        productT[i].addEventListener("click", function (event) {
             var cartDelete = event.target
             var cartitemR = cartDelete.parentElement.parentElement
             cartitemR.remove()
-            carttotal ()
+            carttotal()
         })
     }
 }
 // ----------------------------Input-change------------------
-function inputchange (){
+function inputchange() {
     var cartItem = document.querySelectorAll('tbody tr')
-    for (var i=0;i<cartItem.length;i++){
-        var inputValue = cartItem[i].querySelector('input') 
-        inputValue.addEventListener("change",function(){
-        carttotal()
+    for (var i = 0; i < cartItem.length; i++) {
+        var inputValue = cartItem[i].querySelector('input')
+        inputValue.addEventListener("change", function () {
+            carttotal()
         })
     }
 }
 // --------------------------------cart----------------------------------------
 // Get the button, and when the user clicks on it, execute myFunction
-document.getElementById("myBtn").onclick = function() {myFunction()};
+document.getElementById("myBtn").onclick = function () { myFunction() };
 
 /* myFunction toggles between adding and removing the show class, which is used to hide and show the dropdown content */
 function myFunction() {
-  document.getElementById("myDropdown").classList.toggle("show");
+    document.getElementById("myDropdown").classList.toggle("show");
 }
 
+
+window.onload = function () {
+    document.onclick = function (e) {
+        if (e.target.id !== 'myDropdown' && e.target.id !== 'myBtn' && e.target.id !== 'cart-click') {
+            document.getElementById("myDropdown").classList.remove("show");
+        }
+    };
+};
+
+// click outside
+
+
+// $("#myBtn").click(function (e) {
+//     $("#myDropdown").show();
+//     e.stopPropagation();
+//     $("#myDropdown").click(function (e) {
+//         e.stopPropagation();
+//     });
+//     $(document).click(function () {
+//         $("#myDropdown").hide();
+
+//     });
+//     $("#myBtn").click(function () {
+//         $("#myDropdown").hide();
+
+
+//     });
+// });
+
+// $(document).ready(function(){
+//     $("#myBtn").click(function(){
+//         $("#myDropdown").slideToggle("show");
+//     });
+// });
+// $(document).click(function (e){
+//     var outcart = $("#myDropdown");
+//     if (!outcart.is(e.target) && outcart.has(e.target).length === 0)
+//     {
+//         outcart.hide();
+//     }
+// });
+
+// // cách nào đó khác
+//     document.getElementById("myDropdown").style.display = "none";
+
+// function showcart(){
+//   var x = document.getElementById("myDropdown");
+//   if(x.style.display === "block"){
+//     x.style.display = "none";
+//   }
+//   else{
+//     x.style.display = "block";
+//   }
+// }
 
 // $('tbody').on('click', '.minus',function () {
 //     var $input = $(this).parent().find('input');
@@ -117,5 +173,49 @@ function myFunction() {
 //     return false;
 //   });
 
-{/* <a href="#" class="plus is-form">+</a>
-<a href="#" class="minus is-form">-</a> */}
+
+if ($('.click-cosmetic').length > 0) {
+    $(document).on('click', '.click-cosmetic', function (e) {
+        $(this).closest('.click-cosmetic').find('.cosmetic-in').addClass('cosmetic_in-active');
+        e.preventDefault();
+    })
+} $(document).click(function (e) {
+    var elm = $(e.target);
+    if (!elm.closest('.click-cosmetic').length) {
+        $('.cosmetic-in').removeClass('cosmetic_in-active')
+    }
+})
+$('.click-cosmetic').click(function (e) {
+    $('.cosmetic-in').toggleClass('cosmetic_in-active');
+    e.preventDefault();
+})
+
+
+if ($('.click-cosmetic').length > 0) {
+    $(document).on('click', '.click-cosmetic', function (e) {
+        $(this).closest('.click-cosmetic').find('.cosmetic-in').addClass('cosmetic_in-active');
+        e.preventDefault();
+    })
+}
+// click out category
+
+$(".categori-menu").click(function (e) {
+    $(".categori").show();
+    e.stopPropagation();
+    $(".categori").click(function (e) {
+        e.stopPropagation();
+    });
+    $(document).click(function () {
+        $(".categori").hide();
+    });
+});
+
+function showcate(){
+  var x = document.getElementById("categori");
+  if(x.style.display === "block"){
+    x.style.display = "none";
+  }
+  else{
+    x.style.display = "block";
+  }
+}
